cde6a35f19faccc5bb221728d46cf71e8099666b
Connor Kam-Cheong <connorkc42@gmail.com>
2018-12-03 16:40:35 +0000

    Cards can touch in and touch out. This changes the 'in_journey' attribute of the card

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,9 +1,10 @@
 class Oystercard
   MAX_BALANCE = 90
-  attr_reader :balance
+  attr_reader :balance, :in_journey
 
   def initialize
     @balance = 0
+    @in_journey = false
   end
 
   def top_up(amount)
@@ -15,4 +16,15 @@ class Oystercard
     @balance -= amount
   end
 
+  def touch_in
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
+  end
+
+  def in_journey?
+    in_journey
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,24 +5,43 @@ describe Oystercard do
     expect(subject.balance).to eq 0
   end
 
+  it 'is not in a journey by default' do
+    expect(subject).not_to be_in_journey
+  end
+
   describe '#top_up' do
     it 'increases the balance of the card by a specified amount' do
       expect{ subject.top_up(11) }.to change{ subject.balance }.by 11
     end
+  end
+
+  context 'has a maximum balance' do
+    before { subject.top_up(Oystercard::MAX_BALANCE) }
 
     it 'raises an error if the maximum balance is exceeded' do
-      max_balance = Oystercard::MAX_BALANCE
-      message = "Maximum balance is #{max_balance}"
-      subject.top_up(max_balance)
-
+      message = "Maximum balance is #{Oystercard::MAX_BALANCE}"
       expect { subject.top_up(1) }.to raise_error message
+    end
+
+    describe '#deduct' do
+      it 'decreases the balance of the card by a specified amount' do
+        expect{ subject.deduct(1) }.to change{ subject.balance }.by -1
+      end
     end
   end
 
-  describe '#deduct' do
-    it 'decreases the balance of the card by a specified amount' do
-      subject.top_up(11)
-      expect{ subject.deduct(1) }.to change{ subject.balance }.by -1
+  describe '#touch_in' do
+    it 'changes in_journey to true' do
+      subject.touch_in
+      expect(subject).to be_in_journey
+    end
+  end
+
+  describe '#touch_out' do
+    it "changes in_journey to false" do
+      subject.touch_in
+      subject.touch_out
+      expect(subject).not_to be_in_journey
     end
   end
 end

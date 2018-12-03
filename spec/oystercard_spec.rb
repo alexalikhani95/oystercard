c70e9d6ea5923f8b82a0184395810210e780f835
Connor Kam-Cheong <connorkc42@gmail.com>
2018-12-03 15:23:27 +0000

    Oystercard can top_up and update balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,4 +4,8 @@ describe Oystercard do
   it 'has a balance of zero' do
     expect(subject.balance).to eq 0
   end
+
+  it 'tops up the card with money' do
+    expect(subject.top_up(11)).to eq 11 
+  end
 end

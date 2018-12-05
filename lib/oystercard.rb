77765a640dda6fe4dca6d0b55428819175dc3c75
Connor Kam-Cheong <connorkc42@gmail.com>
2018-12-05 15:30:07 +0000

    Refactored oystercard

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -5,17 +5,16 @@ class Oystercard
 
   def initialize
     @balance = 0
-    @in_journey = false
     @entry_station = nil
   end
 
   def top_up(amount)
-    raise "Maximum balance is #{MAX_BALANCE}" if (@balance + amount) > MAX_BALANCE
+    raise "Maximum balance is #{MAX_BALANCE}" if @balance + amount > MAX_BALANCE
     @balance += amount
   end
 
   def touch_in(station)
-    raise "Balance is less than #{Oystercard::MIN_FARE}" if balance < MIN_FARE
+    raise "Balance is less than #{MIN_FARE}" if balance < MIN_FARE
     @entry_station = station
   end
 
